2fda7840ce8ff1c0fa604d0af3441644ebe680f2
Minh Le <beaver700nh@gmail.com>
2026-03-07 23:54:34 -0500

    Slideshow done pretty much

--- src/Slideshow.tsx
+++ src/Slideshow.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react'
+import moment from 'moment'
+
 import './Slideshow.css'
 
+const REFRESH_INTERVAL = moment.duration(1, 'hour').asMilliseconds();
+
+const SLIDESHOW_URL = "https://docs.google.com/presentation/d/e/2PACX-1vROO4Y7tVDDnf0iDH0U3CzeecQxCOctDw_MgXkCrpyKHVRqVSCz0lzpKlyL1LiA_LGWUEX7u585lF1A/embed";
+
 export default function Slideshow() {
-	return (
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRefreshKey(x => x + 1);
+    }, REFRESH_INTERVAL);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
 		<article id="slideshow">
-			<h1>Slideshow</h1>
+      <iframe
+        key={refreshKey}
+        allowFullScreen={true}
+        src={`${SLIDESHOW_URL}?start=true&loop=true&delayms=8000&rm=minimal`}
+      ></iframe>
 		</article>
-	)
+  );
 }
