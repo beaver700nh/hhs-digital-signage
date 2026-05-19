ad1c557e86139918dcd279c34c976d57820a51d1
Minh Le <beaver700nh@gmail.com>
2026-05-18 21:56:52 -0400

    Removed unnecessary env var

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_GAPI_KEY: string
-	readonly VITE_GAPI_CLIENT_ID: string
+  readonly VITE_GAPI_KEY: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
