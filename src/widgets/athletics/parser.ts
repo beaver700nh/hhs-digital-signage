3a81ff407b68709022f5186fdddd2ff4e7b72d13
Minh Le <beaver700nh@gmail.com>
2026-05-24 22:57:15 -0400

    Added athletics backend

--- src/widgets/athletics/parser.ts
+++ src/widgets/athletics/parser.ts
@@ -5,29 +5,101 @@ import * as Regex from './regex'
 
 type Schema = EventsTypeSchema & { success: true }
 type Item = Schema['items'][number]
+type Description = Partial<Record<"Sport" | "Level" | "Team" | "Site" | "Subsite", string>>
 
 export interface AthleticsEvent {
 	when: moment.Moment
 	sport: string
+	level?: string
 	opponent?: string
-	location?: string
+	homeAway?: 'home' | 'away'
+	location: {
+		site?: string
+		subsite?: string
+	}
 }
 
-export default function parseAthletics(data: Schema):
-	any {
+export type AthleticsCalendar = Record<string, {
+		when: moment.Moment
+		events: Omit<AthleticsEvent, 'when'>[]
+	}>;
+
+export default function parseAthletics(data: Schema): AthleticsCalendar {
 	const parsed = data.items
 		.slice(0, 5)
 		.map(parseAthleticsEvent)
 
-	// TODO group by start time
-	return parsed
+	const calendar: AthleticsCalendar = {}
+	for (const item of parsed) {
+		const id = item.when.unix()
+
+		calendar[id] ??= {
+			when: item.when,
+			events: [],
+		}
+
+		calendar[id].events.push(item)
+	}
+
+	return calendar
 }
 
 function parseAthleticsEvent(item: Item): AthleticsEvent {
 	const { summary, description, start } = item
 
+	/*
+	 * Title format: LEVEL SPORT [VS OPPONENT]
+	 * Grab opponent from title, if present
+	 * Grab level and sport from description, but fall back to title if missing
+	 * - In that case, it will be impossible to separate them, so omit level
+	 */
+
+	const summaryMatch = summary.match(Regex.HOME_AWAY)
+	const [, team, homeAway, opponent] = [...summaryMatch ?? []]
+
+	const data = parseDescription(description ?? '')
+
+	const site = removeHighSchool(data.Site)
+	const subsite = removeHighSchool(data.Subsite)
+
 	return {
 		when: moment(start.dateTime),
-		sport: summary,
+		sport: (data.Sport ?? team).replaceAll(Regex.REDUNDANT_SPECIFIER, ''),
+		level: data.Sport != null ? data.Level : undefined,
+		opponent: removeHighSchool(opponent),
+		homeAway:
+			homeAway === 'vs' ? 'home' :
+			homeAway === 'at' ? 'away' :
+			homeAway === '@' ? 'away' :
+			undefined,
+		location: {
+			site,
+			subsite: site == null || subsite == null || site.includes(subsite)
+				? undefined : subsite,
+		},
 	}
+}
+
+function parseDescription(description: string) {
+	const match = description.matchAll(Regex.DESCRIPTION);
+
+	if (match == null)
+		return {}
+
+	return Object.fromEntries(
+		[...match].map(m => m.slice(1)
+			.map(x => x.trim().replace(/\s+/g, ' ')))
+	) as Description
+}
+
+function removeHighSchool(location: string | undefined) {
+	if (location == null)
+		return undefined
+
+	const match = location.match(Regex.HIGH_SCHOOL)?.[1]
+
+	if (match == null)
+		return location
+
+	return `${match} HS`
 }
--- src/widgets/athletics/regex.ts
+++ src/widgets/athletics/regex.ts
@@ -1,3 +1,30 @@
-export const HIGH_SCHOOL = /([\w\s-]+)\s+high(?:\s+school)?/i
-export const REDUNDANT_SPECIFIER = /\s+-\s+(?:team|boys|girls|indoor|outdoor)/i
+/**
+ * For normalizing "High", "High School", and "HS" to the same format
+ */
+export const HIGH_SCHOOL = /([\w\s-]+)\s+(?:high(?:\s+school)?|hs)\b/i
+
+/**
+ * Redundant "team":
+ * | Sport: Tennis - Team
+ * | Level: Girls Varsity
+ *
+ * Redunant "boys":
+ * | Sport: Lacrosse - Boys
+ * | Level: Boys Varsity
+ */
+export const REDUNDANT_SPECIFIER = /\s+-\s+(?:team|boys|girls|indoor|outdoor)/gi
+
+/**
+ * X vs Y (home) or X at Y (away)
+ */
 export const HOME_AWAY = /^(.*)\s+(vs|@|at)\s+(.*)$/i
+
+/**
+ * YAML-like structure for the event description:
+ * | Sport: Track & Field - Outdoor
+ * | Level: Coed 6/7/8th
+ * | Team: Holliston High School
+ * | Site: Medfield HS
+ * | Subsite: MHS Track
+ */
+export const DESCRIPTION = /^\s*(\w+):\s*(.*)\s*$/gm
